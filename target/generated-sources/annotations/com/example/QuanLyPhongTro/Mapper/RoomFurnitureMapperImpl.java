32dabb4c1126e1ff60b08a9e8f7115c67cd4ab96
Just-a-very-smart-AI-system <22010142@st.phenikaa-uni.edu.vn>
2025-04-23 07:47:34 +0700

    api furniture

---
+++ target/generated-sources/annotations/com/example/QuanLyPhongTro/Mapper/RoomFurnitureMapperImpl.java
@@ -0,0 +1,39 @@
+package com.example.QuanLyPhongTro.Mapper;
+
+import com.example.QuanLyPhongTro.DTO.Request.CreateRoomFurnitureRequestDTO;
+import com.example.QuanLyPhongTro.Entity.RoomFurniture;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-04-21T14:27:57+0700",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
+)
+@Component
+public class RoomFurnitureMapperImpl implements RoomFurnitureMapper {
+
+    @Override
+    public RoomFurniture toEntity(CreateRoomFurnitureRequestDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        RoomFurniture roomFurniture = new RoomFurniture();
+
+        roomFurniture.setUtilityName( dto.getUtilityName() );
+
+        return roomFurniture;
+    }
+
+    @Override
+    public RoomFurniture toEntity(CreateRoomFurnitureRequestDTO dto, RoomFurniture roomFurniture) {
+        if ( dto == null ) {
+            return roomFurniture;
+        }
+
+        roomFurniture.setUtilityName( dto.getUtilityName() );
+
+        return roomFurniture;
+    }
+}
